11f40e5be53cb3eb48235998b490e9a13a0e052a
Marc Bux <buxmarcn@informatik.hu-berlin.de>
2014-12-18 17:43:32 +0100

    Merged changes from Hi-WAY repo

--- recipes/variantcall.cf.rb
+++ recipes/variantcall.cf.rb
@@ -12,26 +12,22 @@ template "#{node[:hiway][:home]}/#{node[:hiway][:variantcall][:workflow]}" do
   mode "0774"
 end
 
-prepared_variantcall = "/tmp/.prepared_variantcall"
 bash "prepare_variantcall" do
   user node[:hiway][:user]
   group node[:hiway][:group]
   code <<-EOF
   set -e && set -o pipefail
-  #{node[:hadoop][:home]}/bin/yarn jar #{node[:hiway][:home]}/hiway-core-#{node[:hiway][:version]}.jar -w #{node[:hiway][:home]}/#{node[:hiway][:variantcall][:setupworkflow]}}
-  touch #{prepared_variantcall}
+  #{node[:hadoop][:home]}/bin/yarn jar #{node[:hiway][:home]}/hiway-core-#{node[:hiway][:version]}.jar -w #{node[:hiway][:home]}/#{node[:hiway][:variantcall][:setupworkflow]}} -s #{node[:hiway][:home]}/variantcall_setup_summary.json
   EOF
 #    not_if { ::File.exists?( "#{prepared_variantcall}" ) }
 end
 
-ran_variantcall = "/tmp/.ran_variantcall"
 bash "run_variantcall" do
   user node[:hiway][:user]
   group node[:hiway][:group]
   code <<-EOF
   set -e && set -o pipefail
-  #{node[:hadoop][:home]}/bin/yarn jar #{node[:hiway][:home]}/hiway-core-#{node[:hiway][:version]}.jar -w #{node[:hiway][:home]}/#{node[:hiway][:variantcall][:workflow]}}
-  touch #{ran_variantcall}
+  #{node[:hadoop][:home]}/bin/yarn jar #{node[:hiway][:home]}/hiway-core-#{node[:hiway][:version]}.jar -w #{node[:hiway][:home]}/#{node[:hiway][:variantcall][:workflow]}} -s #{node[:hiway][:home]}/variantcall_summary.json
   EOF
 #    not_if { ::File.exists?( "#{ran_variantcall}" ) }
 end
