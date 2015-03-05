30435fa4d2bb4a6fe46333788f4c7684afa9c572
Marc Bux <buxmarcn@informatik.hu-berlin.de>
2015-03-05 15:07:28 +0100

    Added SRA toolkit to RNASeq workflow

--- recipes/RNASeq_client.rb
+++ recipes/RNASeq_client.rb
@@ -1,3 +1,50 @@
+# download SRA toolkit binaries
+remote_file "#{Chef::Config[:file_cache_path]}/#{node[:hiway][:RNASeq][:sratoolkit][:targz]}" do
+  source node[:hiway][:RNASeq][:sratoolkit][:url]
+  owner node[:hiway][:user]
+  group node[:hadoop][:group]
+  mode "775"
+  action :create_if_missing
+end
+
+# install SRA toolkit
+bash "install_sratoolkit" do
+  user node[:hiway][:user]
+  group node[:hadoop][:group]
+  code <<-EOH
+  set -e && set -o pipefail
+    tar xvfz #{Chef::Config[:file_cache_path]}/#{node[:hiway][:RNASeq][:sratoolkit][:targz]} -C #{node[:hiway][:software][:dir]}
+  EOH
+  not_if { ::File.exist?("#{node[:hiway][:RNASeq][:sratoolkit][:home]}") }
+end
+
+# add SRA toolkit executables to /usr/bin
+bash 'update_env_variables' do
+  user "root"
+  code <<-EOH
+  set -e && set -o pipefail
+    ln -s #{node[:hiway][:RNASeq][:sratoolkit][:home]}/bin/* /usr/bin/
+  EOH
+  not_if { ::File.exist?("/usr/bin/vdb-config") }
+end
+
+# create SRA toolkit configuration directory
+directory "#{node[:hiway][:home]}/.ncbi" do
+  owner node[:hiway][:user]
+  group node[:hadoop][:group]
+  mode "755"
+  action :create
+  recursive true
+end
+
+# create SRA configuration
+template "#{node[:hiway][:home]}/.ncbi/user-settings.mkfg" do
+  user node[:hiway][:user]
+  group node[:hadoop][:group]
+  source "user-settings.mkfg.erb"
+  mode "775"
+end
+
 # prepare the RNASeq workflow file
 template "#{node[:hiway][:home]}/#{node[:hiway][:RNASeq][:workflow]}" do
   user node[:hiway][:user]
@@ -7,88 +54,42 @@ template "#{node[:hiway][:home]}/#{node[:hiway][:RNASeq][:workflow]}" do
 end
 
 # prepare the mouse musculus reference annotation for Cufflinks etc.
-template "#{Chef::Config[:file_cache_path]}/#{node[:hiway][:RNASeq][:ref_annotation][:targz]}" do
-  user node[:hiway][:user]
-  group node[:hadoop][:group]
-  source "#{node[:hiway][:RNASeq][:ref_annotation][:targz]}.erb"
-  mode "775"
-end
-
-# prepare input: sample 1, replicate 1
-remote_file "#{node[:hiway][:home]}/#{node[:hiway][:RNASeq][:input1][:recplicate1][:fastq]}" do
-  source "#{node[:hiway][:RNASeq][:input1][:recplicate1][:url]}"
+cookbook_file "#{Chef::Config[:file_cache_path]}/#{node[:hiway][:RNASeq][:ref_annotation][:targz]}" do
+  source "#{node[:hiway][:RNASeq][:ref_annotation][:targz]}"
   owner node[:hiway][:user]
   group node[:hadoop][:group]
   mode "775"
   action :create_if_missing
 end
 
-# prepare input: sample 1, replicate 2
-remote_file "#{node[:hiway][:home]}/#{node[:hiway][:RNASeq][:input1][:recplicate2][:fastq]}" do
-  source "#{node[:hiway][:RNASeq][:input1][:recplicate2][:url]}"
-  owner node[:hiway][:user]
-  group node[:hadoop][:group]
-  mode "775"
-  action :create_if_missing
-end
-
-# prepare input: sample 1, replicate 3
-remote_file "#{node[:hiway][:home]}/#{node[:hiway][:RNASeq][:input1][:recplicate3][:fastq]}" do
-  source "#{node[:hiway][:RNASeq][:input1][:recplicate3][:url]}"
-  owner node[:hiway][:user]
-  group node[:hadoop][:group]
-  mode "775"
-  action :create_if_missing
-end
-
-# prepare input: sample 2, replicate 1
-remote_file "#{node[:hiway][:home]}/#{node[:hiway][:RNASeq][:input2][:recplicate1][:fastq]}" do
-  source "#{node[:hiway][:RNASeq][:input2][:recplicate1][:url]}"
-  owner node[:hiway][:user]
-  group node[:hadoop][:group]
-  mode "775"
-  action :create_if_missing
-end
-
-# prepare input: sample 2, replicate 2
-remote_file "#{node[:hiway][:home]}/#{node[:hiway][:RNASeq][:input2][:recplicate2][:fastq]}" do
-  source "#{node[:hiway][:RNASeq][:input2][:recplicate2][:url]}"
-  owner node[:hiway][:user]
-  group node[:hadoop][:group]
-  mode "775"
-  action :create_if_missing
-end
-
-# prepare input: sample 2, replicate 3
-remote_file "#{node[:hiway][:home]}/#{node[:hiway][:RNASeq][:input2][:recplicate3][:fastq]}" do
-  source "#{node[:hiway][:RNASeq][:input2][:recplicate3][:url]}"
-  owner node[:hiway][:user]
-  group node[:hadoop][:group]
-  mode "775"
-  action :create_if_missing
-end
-
-# copy input data into hdfs
+# obtain fastq data and copy input data into hdfs
 bash "stage_out_input_data" do
   user node[:hiway][:user]
   group node[:hadoop][:group]
   code <<-EOH
   set -e && set -o pipefail
     tar xzvf #{Chef::Config[:file_cache_path]}/#{node[:hiway][:RNASeq][:ref_annotation][:targz]} -C #{node[:hiway][:home]}
+    fastq-dump -O #{node[:hiway][:home]} #{node[:hiway][:RNASeq][:input1][:replicate1][:accession]}
+    fastq-dump -O #{node[:hiway][:home]} #{node[:hiway][:RNASeq][:input1][:replicate2][:accession]}
+    fastq-dump -O #{node[:hiway][:home]} #{node[:hiway][:RNASeq][:input1][:replicate3][:accession]}
+    fastq-dump -O #{node[:hiway][:home]} #{node[:hiway][:RNASeq][:input2][:replicate1][:accession]}
+    fastq-dump -O #{node[:hiway][:home]} #{node[:hiway][:RNASeq][:input2][:replicate2][:accession]}
+    fastq-dump -O #{node[:hiway][:home]} #{node[:hiway][:RNASeq][:input2][:replicate3][:accession]}
     #{node[:hadoop][:home]}/bin/hdfs dfs -put #{node[:hiway][:home]}/#{node[:hiway][:RNASeq][:ref_annotation][:gtf]} #{node[:hiway][:hiway][:hdfs][:basedir]}
-    #{node[:hadoop][:home]}/bin/hdfs dfs -put #{node[:hiway][:home]}/#{node[:hiway][:RNASeq][:input1][:recplicate1][:fastq]} #{node[:hiway][:hiway][:hdfs][:basedir]}
-    #{node[:hadoop][:home]}/bin/hdfs dfs -put #{node[:hiway][:home]}/#{node[:hiway][:RNASeq][:input1][:recplicate2][:fastq]} #{node[:hiway][:hiway][:hdfs][:basedir]}
-    #{node[:hadoop][:home]}/bin/hdfs dfs -put #{node[:hiway][:home]}/#{node[:hiway][:RNASeq][:input1][:recplicate3][:fastq]} #{node[:hiway][:hiway][:hdfs][:basedir]}
-    #{node[:hadoop][:home]}/bin/hdfs dfs -put #{node[:hiway][:home]}/#{node[:hiway][:RNASeq][:input2][:recplicate1][:fastq]} #{node[:hiway][:hiway][:hdfs][:basedir]}
-    #{node[:hadoop][:home]}/bin/hdfs dfs -put #{node[:hiway][:home]}/#{node[:hiway][:RNASeq][:input2][:recplicate2][:fastq]} #{node[:hiway][:hiway][:hdfs][:basedir]}
-    #{node[:hadoop][:home]}/bin/hdfs dfs -put #{node[:hiway][:home]}/#{node[:hiway][:RNASeq][:input2][:recplicate3][:fastq]} #{node[:hiway][:hiway][:hdfs][:basedir]}
+    #{node[:hadoop][:home]}/bin/hdfs dfs -put #{node[:hiway][:home]}/#{node[:hiway][:RNASeq][:input1][:replicate1][:accession]}.fastq #{node[:hiway][:hiway][:hdfs][:basedir]}#{node[:hiway][:RNASeq][:input1][:replicate1][:fastq]}
+    #{node[:hadoop][:home]}/bin/hdfs dfs -put #{node[:hiway][:home]}/#{node[:hiway][:RNASeq][:input1][:replicate2][:accession]}.fastq #{node[:hiway][:hiway][:hdfs][:basedir]}#{node[:hiway][:RNASeq][:input1][:replicate2][:fastq]}
+    #{node[:hadoop][:home]}/bin/hdfs dfs -put #{node[:hiway][:home]}/#{node[:hiway][:RNASeq][:input1][:replicate3][:accession]}.fastq #{node[:hiway][:hiway][:hdfs][:basedir]}#{node[:hiway][:RNASeq][:input1][:replicate3][:fastq]}
+    #{node[:hadoop][:home]}/bin/hdfs dfs -put #{node[:hiway][:home]}/#{node[:hiway][:RNASeq][:input2][:replicate1][:accession]}.fastq #{node[:hiway][:hiway][:hdfs][:basedir]}#{node[:hiway][:RNASeq][:input2][:replicate1][:fastq]}
+    #{node[:hadoop][:home]}/bin/hdfs dfs -put #{node[:hiway][:home]}/#{node[:hiway][:RNASeq][:input2][:replicate2][:accession]}.fastq #{node[:hiway][:hiway][:hdfs][:basedir]}#{node[:hiway][:RNASeq][:input2][:replicate2][:fastq]}
+    #{node[:hadoop][:home]}/bin/hdfs dfs -put #{node[:hiway][:home]}/#{node[:hiway][:RNASeq][:input2][:replicate3][:accession]}.fastq #{node[:hiway][:hiway][:hdfs][:basedir]}#{node[:hiway][:RNASeq][:input2][:replicate3][:fastq]}
+    rm -r #{node[:hiway][:home]}/ncbi
     rm #{node[:hiway][:home]}/#{node[:hiway][:RNASeq][:ref_annotation][:gtf]}
-    rm #{node[:hiway][:home]}/#{node[:hiway][:RNASeq][:input1][:recplicate1][:fastq]}
-    rm #{node[:hiway][:home]}/#{node[:hiway][:RNASeq][:input1][:recplicate2][:fastq]}
-    rm #{node[:hiway][:home]}/#{node[:hiway][:RNASeq][:input1][:recplicate3][:fastq]}
-    rm #{node[:hiway][:home]}/#{node[:hiway][:RNASeq][:input2][:recplicate1][:fastq]}
-    rm #{node[:hiway][:home]}/#{node[:hiway][:RNASeq][:input2][:recplicate2][:fastq]}
-    rm #{node[:hiway][:home]}/#{node[:hiway][:RNASeq][:input2][:recplicate3][:fastq]}
+    rm #{node[:hiway][:home]}/#{node[:hiway][:RNASeq][:input1][:replicate1][:fastq]}
+    rm #{node[:hiway][:home]}/#{node[:hiway][:RNASeq][:input1][:replicate2][:fastq]}
+    rm #{node[:hiway][:home]}/#{node[:hiway][:RNASeq][:input1][:replicate3][:fastq]}
+    rm #{node[:hiway][:home]}/#{node[:hiway][:RNASeq][:input2][:replicate1][:fastq]}
+    rm #{node[:hiway][:home]}/#{node[:hiway][:RNASeq][:input2][:replicate2][:fastq]}
+    rm #{node[:hiway][:home]}/#{node[:hiway][:RNASeq][:input2][:replicate3][:fastq]}
   EOH
-  not_if "#{node[:hadoop][:home]}/bin/hdfs dfs -test -e #{node[:hiway][:hiway][:hdfs][:basedir]}#{node[:hiway][:RNASeq][:input2][:recplicate3][:fastq]}"
+  not_if "#{node[:hadoop][:home]}/bin/hdfs dfs -test -e #{node[:hiway][:hiway][:hdfs][:basedir]}#{node[:hiway][:RNASeq][:input2][:replicate3][:fastq]}"
 end
