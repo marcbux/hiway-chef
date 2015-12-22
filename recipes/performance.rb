d90a4be17d4baf8b01ea1b4752c370a5d5eb7dd1
Marc Bux <buxmarcn@informatik.hu-berlin.de>
2015-12-22 14:29:09 +0100

    added performance measurement recipes

---
+++ recipes/performance.rb
@@ -0,0 +1,10 @@
+package "sysstat"
+package "ifstat"
+
+# prepare the variant call workflow file
+template "#{node[:saasfee][:home]}/perf.sh" do
+  user node[:saasfee][:user]
+  group node[:hadoop][:group]
+  source "perf.sh.erb"
+  mode "754"
+end
